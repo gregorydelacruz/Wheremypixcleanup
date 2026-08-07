d5d99b54bd3052d67147b9980c353c091df4cff5
Gregory de la Cruz <80643448+gregorydelacruz@users.noreply.github.com>
2026-08-06 18:42:09 -0700

    Fix accidental sidebar color typo in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,7 +59,7 @@ export default {
 				},
 				sidebar: {
 					DEFAULT: 'hsl(var(--sidebar-background))',
-					foreground: 'hsl(var(--sidebar-primary))',
+					foreground: 'hsl(var(--sidebar-foreground))',
 					primary: 'hsl(var(--sidebar-primary))',
 					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
 					accent: 'hsl(var(--sidebar-accent))',
